f46afe42a2615cb3e67fc0482d9754239fff4a77
Andre Sudi Yudatama <y.andresudi@gmail.com>
2018-05-18 00:48:04 +0700

    Add files via upload

---
+++ Exercise_4_Graduates.js
@@ -0,0 +1,98 @@
+function graduates(students) {
+    // Code disini
+    var lulusan = {}
+    for (var i = 0; i < students.length; i++) {
+        var currentClass = students[i].class
+        
+        if (lulusan[currentClass] === undefined) {
+            lulusan[currentClass] = []
+            //console.log(lulusan)
+        }
+
+        if (students[i].score > 75) {
+            var muridlulus = { 
+                name: students[i].name,
+                score: students[i].score
+            }
+            lulusan[currentClass].push(muridlulus)
+        }
+    }
+    return lulusan
+}
+
+console.log(graduates([
+    {
+        name: 'Dimitri',
+        score: 90,
+        class: 'foxes'
+    },
+    {
+        name: 'Alexei',
+        score: 85,
+        class: 'wolves'
+    },
+    {
+        name: 'Sergei',
+        score: 74,
+        class: 'foxes'
+    },
+    {
+        name: 'Anastasia',
+        score: 78,
+        class: 'wolves'
+    }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+    {
+        name: 'Alexander',
+        score: 100,
+        class: 'foxes'
+    },
+    {
+        name: 'Alisa',
+        score: 76,
+        class: 'wolves'
+    },
+    {
+        name: 'Vladimir',
+        score: 92,
+        class: 'foxes'
+    },
+    {
+        name: 'Albert',
+        score: 71,
+        class: 'wolves'
+    },
+    {
+        name: 'Viktor',
+        score: 80,
+        class: 'tigers'
+    }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
